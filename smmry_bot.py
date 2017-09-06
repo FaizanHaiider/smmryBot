bce7420d2fb6dc4de876e9ed6cf9fe5e02494a43
faizanhaider12 <faizanhaider12@gmail.com>
2017-09-05 23:10:24 -0400

    added full file io features. tested

--- smmry_bot.py
+++ smmry_bot.py
@@ -21,11 +21,28 @@ def load_previous_subm():
         content = f.readlines()
     content = [x.strip('\n') for x in content]
     # printArr(content)
+    content_length = len(content)
+    # print content_length
+    return content, content_length
+
+def write_to_file(arr):
+    file_name = "db.txt"
+    with open(file_name, "w+") as f:
+        for subm in arr:
+            f.write("%s\n" % subm)
 
 
 def main():
     # r = login()
-    submission_ids = load_previous_subm()
+    subms_ids, subms_length = load_previous_subm()
+    most_recent_subm = "0"
+    if subms_length > 0:
+        most_recent_subm = subms_ids[0]
+
+    # print most_recent_subm
+
+
+
 
 
 
