0b5426440d201f705bae4c05c3d33ba2a7570ad9
faizanhaider12 <faizanhaider12@gmail.com>
2017-09-14 17:20:01 -0400

    added search from timestamp. currently returns new posts but also the timestamp post. needs to be fixed

--- smmry_bot.py
+++ smmry_bot.py
@@ -23,26 +23,16 @@ def load_previous_subm():
     with open(file_name) as f:
         content = f.readlines()
     content = [x.strip('\n') for x in content]
+    content = [x.replace(".0", "") for x in content]
     # printArr(content)
     content_length = len(content)
     # print content_length
     return content, content_length
 
-def write_to_file(arr):
+def write_to_file(most_recent_timestamp):
     file_name = "db.txt"
     with open(file_name, "w+") as f:
-        for subm in arr:
-            f.write("%s\n" % subm)
-
-
-# None == remove timestamp
-# 1 == add timestamp
-# 2 == return most recent timestamp
-def time_stamp(value=None):
-    if value == None:
-        # todo
-    elif value == 1:
-        # todo
+        f.write("%s\n" % most_recent_timestamp)
 
 def format_smmry(smmry_response):
     smmry_array = re.split("\+", smmry_response)
@@ -58,7 +48,7 @@ def format_smmry(smmry_response):
 
     return title, smmry
 
-def create_smmry(subm):
+def get_smmry_php(subm):
     subm_url = subm.url
     cmd = "php curlSmmry.php " + subm.url
     php_script = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -70,30 +60,37 @@ def create_smmry(subm):
         comment.reply("An error occured. Sorry about that.\n\n I am a **bot** [PM]() my creator for questions/concerns")
     return title, summary
 
-def create_smmrys(r):
+def create_smmrys(r, most_recent_timestamp):
     subr = r.subreddit("testbotsdankt")
     post_id_list = []
-    temp_timestamp = ""
+    new_timestamp = most_recent_timestamp
+    first_iter = 1
 
-    for subm in subr.new():
+    for subm in subr.submissions(most_recent_timestamp):
         print subm.url
         for top_level_comment in subm.comments:
             if re.search("^(!smmry_bot)$", top_level_comment.body, re.IGNORECASE):
-                title, smmry = create_smmry(subm)
+                title, smmry = get_smmry_php(subm)
             	# create final comment
                 title = "**Title**: [" + title + "](" + subm.url + ") \n\n>"
-            	ending_msg = "\n\n Notice something wrong? Send me a msg. \n\n*I am a bot*"
+            	ending_msg = "\n\n Notice something wrong or want to give feedback? Send me a msg. \n\n*I am a bot*"
                 final_msg = title + smmry + ending_msg
                 top_level_comment.reply(final_msg)
+                if first_iter == 1:
+                    new_timestamp = subm.created_utc
+                    first_iter = 0
+                    print new_timestamp
+    return new_timestamp
 
 
 def main():
     r = login()
     subms_ids, subms_length = load_previous_subm()
-    most_recent_subm = "0"
+    most_recent_timestamp = "0"
     if subms_length > 0:
-        most_recent_subm = subms_ids[0]
-    create_smmrys(r)
+        most_recent_timestamp = subms_ids[0]
+    most_recent_timestamp = create_smmrys(r, most_recent_timestamp)
+    write_to_file(most_recent_timestamp)
 
 
 if __name__ == "__main__":
