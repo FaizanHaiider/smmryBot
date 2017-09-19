24963e08480f9bd07b1533cefc30780196d4c0cf
faizanhaider12 <faizanhaider12@gmail.com>
2017-09-19 13:20:41 -0400

    Fixed double reply for old posts. Added error messages. Added submission id to db.txt.

--- smmry_bot.py
+++ smmry_bot.py
@@ -4,6 +4,9 @@ import praw, re, subprocess, os
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
+ERR_STR = "Sorry, an error occured."
+ENDING_STR = "\n\n Notice something wrong or want to give feedback? Send me a msg. \n\n*I am a bot*"
+
 def printArr(arr):
     for x in arr:
         print x
@@ -20,19 +23,28 @@ def login():
 
 def load_previous_subm():
     file_name = "db.txt"
-    with open(file_name) as f:
-        content = f.readlines()
-    content = [x.strip('\n') for x in content]
-    content = [x.replace(".0", "") for x in content]
-    # printArr(content)
-    content_length = len(content)
-    # print content_length
-    return content, content_length
+    old_timestamp = 0
+    old_id = 0
 
-def write_to_file(most_recent_timestamp):
+    with open(file_name) as f:
+        content = f.readline()
+
+    if len(content) == 0:
+        return old_timestamp, old_id
+
+    content = content.strip("\n")
+    content_split = content.split(":")
+
+    old_timestamp = content_split[0]
+    old_timestamp = old_timestamp.replace(".0", "")
+    old_id = content_split[1]
+
+    return old_timestamp, old_id
+
+def write_to_file(old_timestamp, old_id):
     file_name = "db.txt"
     with open(file_name, "w+") as f:
-        f.write("%s\n" % most_recent_timestamp)
+        f.write("%s:%s\n" % (old_timestamp, old_id))
 
 def format_smmry(smmry_response):
     smmry_array = re.split("\+", smmry_response)
@@ -54,43 +66,61 @@ def get_smmry_php(subm):
     php_script = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     smmry_response = php_script.stdout.read()
 
+    # default title and summary values
+    title = "error"
+    summary = "error"
+
     try:
         title, summary = format_smmry(smmry_response)
     except:
-        comment.reply("An error occured. Sorry about that.\n\n I am a **bot** [PM]() my creator for questions/concerns")
+        pass
+
     return title, summary
 
-def create_smmrys(r, most_recent_timestamp):
+def create_smmrys(r, old_timestamp, old_id):
     subr = r.subreddit("testbotsdankt")
     post_id_list = []
-    new_timestamp = most_recent_timestamp
+    new_timestamp = old_timestamp
+    new_subm_id = old_id
     first_iter = 1
 
-    for subm in subr.submissions(most_recent_timestamp):
-        print subm.url
+    for subm in subr.submissions(old_timestamp):
+        if subm.id == old_id:
+            continue
         for top_level_comment in subm.comments:
             if re.search("^(!smmry_bot)$", top_level_comment.body, re.IGNORECASE):
                 title, smmry = get_smmry_php(subm)
-            	# create final comment
-                title = "**Title**: [" + title + "](" + subm.url + ") \n\n>"
-            	ending_msg = "\n\n Notice something wrong or want to give feedback? Send me a msg. \n\n*I am a bot*"
-                final_msg = title + smmry + ending_msg
-                top_level_comment.reply(final_msg)
+
+                # error check
+                if title == "error" and summary == "error":
+                    err_msg = ERR_STR + ENDING_STR
+                    top_level_comment.reply(err_msg)
+                else:
+                    # create final comment
+                    title = "**Title**: [" + title + "](" + subm.url + ") \n\n>"
+                    final_msg = title + smmry + ENDING_STR
+                    # reply to !smmry_bot comment
+                    top_level_comment.reply(final_msg)
+
+                # get new timestamp and id for next iteration
                 if first_iter == 1:
                     new_timestamp = subm.created_utc
+                    new_subm_id = subm.id
                     first_iter = 0
+                    print "new: "
                     print new_timestamp
-    return new_timestamp
+                    print new_subm_id
+
+    return new_timestamp, new_subm_id
 
 
 def main():
     r = login()
-    subms_ids, subms_length = load_previous_subm()
-    most_recent_timestamp = "0"
-    if subms_length > 0:
-        most_recent_timestamp = subms_ids[0]
-    most_recent_timestamp = create_smmrys(r, most_recent_timestamp)
-    write_to_file(most_recent_timestamp)
+    old_timestamp, old_id = load_previous_subm()
+
+    new_timestamp, new_id = create_smmrys(r, old_timestamp, old_id)
+
+    write_to_file(new_timestamp, new_id)
 
 
 if __name__ == "__main__":
